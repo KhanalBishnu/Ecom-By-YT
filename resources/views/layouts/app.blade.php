a123fb97ae14b5c287101da63a7f4f60e8b24550
Bishnu <khanalbishnu333@gmail.com>
2023-05-08 23:03:20 +0545

    text add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -125,6 +125,8 @@
     {{-- owl carousel link js  --}}
     <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
     {{-- Exzoom product image link js  --}}
 
     <script src="{{ asset('assets/exzoom/jquery.exzoom.js') }}"></script>
